2b36d5734a19fbd3f180676b91f1cec94d5f7ea3
gita sintya <35203333+gitasnty@users.noreply.github.com>
2022-10-05 11:58:23 +0700

    add button upload

--- resources/views/backend/catatanmutu/view_catatanmutu.blade.php
+++ resources/views/backend/catatanmutu/view_catatanmutu.blade.php
@@ -46,9 +46,19 @@
 								<td><div><input type="file" class="btn btn-app btn-primary "></div></td>
 								<td>done</td>
 								<td>4011/04/25</td>
-								<td><button href="#" type="button" class="btn btn-social-icon btn-google">Delete</button></td>
+								<td>
+                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                        Upload
+                                      </button>
+                                      {{-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                        Launch demo modal
+                                      </button> --}}
+
+
+                                    <button href="#" type="button" class="btn btn-social-icon btn-google">Delete</button>
+                                </td>
 							</tr>
-						</tbody>			  
+						</tbody>
 
 					</table>
 					<div>
@@ -73,4 +83,26 @@
   </div>
 
 
+
 @endsection
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
