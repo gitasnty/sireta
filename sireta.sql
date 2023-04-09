aaf5e55e11b7f3358a6e12fad17ff0e7d2b63d8c
gita sintya <35203333+gitasnty@users.noreply.github.com>
2023-04-10 05:21:50 +0700

    Update sireta.sql

--- sireta.sql
+++ sireta.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Apr 2023 pada 02.02
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 8.1.6
+-- Generation Time: Apr 10, 2023 at 12:15 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `documents`
+-- Table structure for table `documents`
 --
 
 CREATE TABLE `documents` (
@@ -40,10 +40,29 @@ CREATE TABLE `documents` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `documents`
+--
+
+INSERT INTO `documents` (`id`, `code`, `name`, `doctype`, `standard`, `period`, `file_path`, `user_id`, `created_at`, `updated_at`) VALUES
+(89, '1', '1', 'Catatan Mutu', 'Standar 1', 2001, 'upload/TkNjPrijMu0nXI3HmhSIaBrBUxmBQax3bOSq3vLj.jpg', 34, '2023-04-02 18:59:53', '2023-04-06 20:34:19'),
+(91, '1', '1', 'Lain Lain', NULL, 2001, 'upload/5GN1ZtZs9qlLlOAkNupyzCBMl72siGN5OPnsLjUq.doc', 34, '2023-04-02 19:08:59', '2023-04-07 00:49:48'),
+(92, '1', '1', 'Manual Mutu', 'Standar 1', 2001, 'upload/EO8mY1R0miQmpHRqD5eHAg3qo31O1l6LN41RJY4h.pdf', 34, '2023-04-02 19:14:55', '2023-04-07 03:17:21'),
+(93, '1', '1', 'Prosedur Mutu', 'Standar 1', 2001, NULL, 34, '2023-04-02 19:15:32', '2023-04-02 19:15:32'),
+(94, '1', '1', 'Petunjuk Kerja', 'Standar 1', 2001, 'upload/T38SKinZELFhncvVuNBz9feXr4UCyyz2764BYfV9.pdf', 34, '2023-04-02 19:25:55', '2023-04-07 00:44:16'),
+(95, '2', '2', 'Manual Mutu', 'Standar 2', 2020, NULL, 34, '2023-04-06 20:57:47', '2023-04-06 20:57:47'),
+(96, '1', 'Data Prestasi Kelas X RPL 2', 'Manual Mutu', 'Standar 1', 2020, NULL, 34, '2023-04-06 22:01:36', '2023-04-06 22:01:36'),
+(97, '1', '1', 'Manual Mutu', 'Standar 1', 2019, NULL, 34, '2023-04-06 22:23:41', '2023-04-06 22:23:41'),
+(98, '2', '2', 'Manual Mutu', 'Standar 1', 2019, NULL, 34, '2023-04-06 22:23:49', '2023-04-06 22:23:49'),
+(99, '3', '3', 'Manual Mutu', 'Standar 1', 2003, NULL, 34, '2023-04-06 22:23:56', '2023-04-06 22:23:56'),
+(100, '4', '4', 'Manual Mutu', 'Standar 1', 2004, NULL, 34, '2023-04-06 22:24:12', '2023-04-06 22:24:12'),
+(101, '123', '123', 'Manual Mutu', 'Standar 1', 2012, NULL, 34, '2023-04-07 03:40:44', '2023-04-07 03:40:44'),
+(102, '123', '123', 'Prosedur Mutu', 'Standar 1', 2020, NULL, 34, '2023-04-07 03:41:00', '2023-04-07 03:41:00');
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `failed_jobs`
+-- Table structure for table `failed_jobs`
 --
 
 CREATE TABLE `failed_jobs` (
@@ -59,7 +78,7 @@ CREATE TABLE `failed_jobs` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `migrations`
+-- Table structure for table `migrations`
 --
 
 CREATE TABLE `migrations` (
@@ -69,7 +88,7 @@ CREATE TABLE `migrations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `migrations`
+-- Dumping data for table `migrations`
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
@@ -86,7 +105,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `password_resets`
+-- Table structure for table `password_resets`
 --
 
 CREATE TABLE `password_resets` (
@@ -98,7 +117,7 @@ CREATE TABLE `password_resets` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `personal_access_tokens`
+-- Table structure for table `personal_access_tokens`
 --
 
 CREATE TABLE `personal_access_tokens` (
@@ -116,7 +135,7 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `sessions`
+-- Table structure for table `sessions`
 --
 
 CREATE TABLE `sessions` (
@@ -129,20 +148,18 @@ CREATE TABLE `sessions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `sessions`
+-- Dumping data for table `sessions`
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('bKStQSgeFYJhtZUYTGIxgDfOc7UZxtotmRp90SqG', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36', 'YTo1OntzOjM6InVybCI7YTowOnt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sb2dpbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NjoiX3Rva2VuIjtzOjQwOiIzcDhzSDB4SXBCOUtPVjhRcDBOS2JKSDNyZWloWWY5V0dEbUZvMnZUIjtzOjIxOiJwYXNzd29yZF9oYXNoX3NhbmN0dW0iO3M6NjA6IiQyeSQxMCQxRlo3b2w2QVhsOExrTnpSS3lLLnp1bkEzc1dLaWNUSmtmTUdJRTlzaWgwMmxrR1FCUG4zVyI7fQ==', 1680112435),
-('IcBuR3HPm1urKgbJUsjnKvRNkPvNo4Xdmf90ZWoz', 34, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36', 'YTo2OntzOjM6InVybCI7YTowOnt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzY6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC91bml0a2VyamEvdmlldyI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NjoiX3Rva2VuIjtzOjQwOiJxOVBCaEZtOWExWThVTllob292WU80OE1rbE1yOUtGMUh6a1dlbWdHIjtzOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aTozNDtzOjIxOiJwYXNzd29yZF9oYXNoX3NhbmN0dW0iO3M6NjA6IiQyeSQxMCR2V3dJM0FKanEzTU8yaTgzRVNuWENPNmhjcTNIdVBjOEJQLjhkck1qU1ltNi5oMVN3c0lPRyI7fQ==', 1680480023),
-('k7aci8sVwLvSpNsEPpWLNTqYrYDyYozKKoW4G3xn', 28, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiWlFFaFc0VmxVSnlDU3NtZXA0MlR1MkZ1Q1I4QjFDZnZwTUlTT1VBNyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzU6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sYWlubGFpbi92aWV3Ijt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6Mjg7czoyMToicGFzc3dvcmRfaGFzaF9zYW5jdHVtIjtzOjYwOiIkMnkkMTAkVUUwaS42ZUVPUGY0N0dxaUdma3lIT0FUeVhaenZtSFY4MTdxWFByaTdqQXIxVklnaFdEVmEiO30=', 1680232353),
-('QbickqER8TRkHmZqkSWuXFEpKDBFVObxGbBtXRz8', 31, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiRWpkWlBLbVBVT05RRGlWR0lOZjMxeGRzcU5UQjExOFoxaGdFMTNHWSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mzc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9tYW51YWxtdXR1L3ZpZXciO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aTozMTtzOjIxOiJwYXNzd29yZF9oYXNoX3NhbmN0dW0iO3M6NjA6IiQyeSQxMCQxRlo3b2w2QVhsOExrTnpSS3lLLnp1bkEzc1dLaWNUSmtmTUdJRTlzaWgwMmxrR1FCUG4zVyI7fQ==', 1680232526),
-('UXQ16SDbYQCsIPFIBMp5COtJWl9Ja2QeUNWTW4EY', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiQjRIcGZaSmZ4RmJnQWRrWlh6QlFQNDV3ZlFndXl6R0hZUHN3d21KWSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sb2dpbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6MjE6InBhc3N3b3JkX2hhc2hfc2FuY3R1bSI7czo2MDoiJDJ5JDEwJHJkVWloYVFSdDQ1cjJINHlHLktnWmUvSUFmV2RuWTRBNG0zTUJkcHJSTUpPYXJ2UjZVOGZLIjt9', 1680111926);
+('8NDZ8gnRisUbbsZfPiztVMLdtGS2PysiC9JbovnZ', 36, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoib3hOVFFqQ2tWQlBCdlkyNENEdkZmclNVSGdkY01LYklHbzNoTHdUQSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYXNoYm9hcmQiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aTozNjtzOjIxOiJwYXNzd29yZF9oYXNoX3NhbmN0dW0iO3M6NjA6IiQyeSQxMCR0NzFaR253bjFjMVY2V3BpczlVUFlPdE0udHZxL1ZMemNxdzBCbHcybHBsRTk1QnBjWm5GQyI7fQ==', 1680859717),
+('OaOuRsJ0ATimkZLBgEcFQpS7n1zk1DVMIRxIpOXJ', 35, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36', 'YTo1OntzOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czozMToiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2Rhc2hib2FyZCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NjoiX3Rva2VuIjtzOjQwOiJVZmRyZmQxdHlHWEg1a1hmNjU1eVBVVFRoRmZkRzFYeHVKRjNiTlpoIjtzOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aTozNTtzOjIxOiJwYXNzd29yZF9oYXNoX3NhbmN0dW0iO3M6NjA6IiQyeSQxMCRid2pERjgua09VWWFmV2gxZnlKbHNPQk1hcllRSGp4dmZwdVlRZGFJVWdzbmZ6ZGp1YzhuNiI7fQ==', 1680864382),
+('qilenMldAYbMabEOyj29vJfEZJnKk10loFRsraPL', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36', 'YTozOntzOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czoyNzoiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2xvZ2luIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo2OiJfdG9rZW4iO3M6NDA6InNIckh6MTFCNzJwR29BakVORGY0ZnlKM0tRNXIxSU45eUJ1WXlUeVMiO30=', 1680864154);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -164,44 +181,46 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `usertype`, `name`, `pj`, `email`, `email_verified_at`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `two_factor_confirmed_at`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`) VALUES
-(34, 'admin', 'admin', 'admin', 'admin', NULL, '$2y$10$vWwI3AJjq3MO2i83ESnXCO6hcq3HuPc8BP.8drMjSYm6.h1SwsIOG', NULL, NULL, NULL, NULL, NULL, NULL, '2023-04-02 16:59:49', '2023-04-02 16:59:49');
+(34, 'admin', 'admin', 'admin', 'admin', NULL, '$2y$10$vWwI3AJjq3MO2i83ESnXCO6hcq3HuPc8BP.8drMjSYm6.h1SwsIOG', NULL, NULL, NULL, NULL, NULL, NULL, '2023-04-02 16:59:49', '2023-04-02 16:59:49'),
+(35, 'user', 'WKS1', 'WKS1', 'wks1', NULL, '$2y$10$bwjDF8.kOUYafWh1fyJlsOBMarYQHjxvfpuYQdaIUgsnfzdjuc8n6', NULL, NULL, NULL, NULL, NULL, NULL, '2023-04-02 18:16:54', '2023-04-02 18:17:15'),
+(37, 'guest', 'Guest', 'Guest', 'Guest', NULL, '$2y$10$RUe4ofJM29HXQk.K0JcYEulz9gNHAS8rdYrh7f9fUOdd.PCqXq9UW', NULL, NULL, NULL, NULL, NULL, NULL, '2023-04-07 02:54:48', '2023-04-07 02:54:48');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `documents`
+-- Indexes for table `documents`
 --
 ALTER TABLE `documents`
   ADD PRIMARY KEY (`id`),
   ADD KEY `documents_user_id_foreign` (`user_id`);
 
 --
--- Indeks untuk tabel `failed_jobs`
+-- Indexes for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
 
 --
--- Indeks untuk tabel `migrations`
+-- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `password_resets`
+-- Indexes for table `password_resets`
 --
 ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
--- Indeks untuk tabel `personal_access_tokens`
+-- Indexes for table `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
   ADD PRIMARY KEY (`id`),
@@ -209,7 +228,7 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
--- Indeks untuk tabel `sessions`
+-- Indexes for table `sessions`
 --
 ALTER TABLE `sessions`
   ADD PRIMARY KEY (`id`),
@@ -217,7 +236,7 @@ ALTER TABLE `sessions`
   ADD KEY `sessions_last_activity_index` (`last_activity`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
@@ -225,45 +244,45 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `username` (`name`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `documents`
+-- AUTO_INCREMENT for table `documents`
 --
 ALTER TABLE `documents`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
--- AUTO_INCREMENT untuk tabel `failed_jobs`
+-- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `migrations`
+-- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT untuk tabel `personal_access_tokens`
+-- AUTO_INCREMENT for table `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `documents`
+-- Constraints for table `documents`
 --
 ALTER TABLE `documents`
   ADD CONSTRAINT `documents_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
